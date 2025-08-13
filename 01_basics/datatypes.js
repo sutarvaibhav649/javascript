266c6483eea6f7af158649dd4f3d6afa8b1bc3dd
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-08-13 12:33:07 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all the JS code as newer version
+
+// alert(3+3) // we are using the nodejs, not browser
+
+
+/* code readability should be high */
+console.log(3+3);
+console.log("Vaibhav");
+
+/*
+    Datatypes
+
+    number => 2^52
+    bigint
+    string => " "
+    boolean => true or false
+    null => standalone value
+    undefined =>
+    symbol => uniqueness
+
+
+    objects
+
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
