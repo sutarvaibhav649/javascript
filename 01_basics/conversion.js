0f88d0ffe4909347c0d79b929465ab4c47b490f7
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-08-13 12:42:01 +0530

    conversion datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,34 @@
+let score = true;
+
+console.log(typeof score);
+
+let valuesInNumbers = Number(score)
+
+console.log(typeof valuesInNumbers);
+console.log(valuesInNumbers);
+
+
+/*
+    "30" => 30
+    "30abc" => NaN
+    true => 1
+ */
+
+let isLoggedIn = "Vaibhav"
+
+let booleanIsLoggedin = Boolean(isLoggedIn)
+console.log(booleanIsLoggedin);
+
+/*
+    1 => true
+    "" => false
+    "Vaibahv" => true
+*/
+
+let someNumber = 12
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
