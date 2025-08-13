658a27dafc2c2f4e96b614ade85702d030311a42
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-08-13 13:20:30 +0530

    operations

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,34 +1,51 @@
-let score = true;
+// let score = true;
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valuesInNumbers = Number(score)
+// let valuesInNumbers = Number(score)
 
-console.log(typeof valuesInNumbers);
-console.log(valuesInNumbers);
-
-
-/*
-    "30" => 30
-    "30abc" => NaN
-    true => 1
- */
-
-let isLoggedIn = "Vaibhav"
-
-let booleanIsLoggedin = Boolean(isLoggedIn)
-console.log(booleanIsLoggedin);
-
-/*
-    1 => true
-    "" => false
-    "Vaibahv" => true
-*/
-
-let someNumber = 12
-let stringNumber = String(someNumber)
-
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(typeof valuesInNumbers);
+// console.log(valuesInNumbers);
 
 
+// /*
+//     "30" => 30
+//     "30abc" => NaN
+//     true => 1
+//  */
+
+// let isLoggedIn = "Vaibhav"
+
+// let booleanIsLoggedin = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedin);
+
+// /*
+//     1 => true
+//     "" => false
+//     "Vaibahv" => true
+// */
+
+// let someNumber = 12
+// let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//********************************** OPERATIONS ******************************* */
+
+let value = 3
+let negValue = -value
+
+
+let str1 = "Hello"
+let str2 = " Vaibhav"
+
+let str3 = str1+str2
+
+console.log(str3);
+
+
+console.log(2+"1");
+console.log("2"+1);
+console.log(2+1+"36");
