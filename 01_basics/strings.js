0a196e2d8ee4e1e741c78134501be10b007f30ba
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-08-14 20:40:29 +0530

    array

---
+++ 01_basics/strings.js
@@ -0,0 +1,20 @@
+const name = "Vaibhav"
+const repoCount = 10
+
+// console.log(name+repoCount+" Value");
+
+const myName = new String("Vaibhav")
+
+console.log(myName);
+console.log(myName.__proto__);
+console.log(myName.length);
+console.log(myName.toLowerCase());
+console.log(myName.charAt(2));
+console.log(myName.indexOf('V'));
+
+
+
+
+
+
+
